6a7cca97ef251fc393a98f6384f939c5d5d23845
felix sun <sunguiquan1980@sina.com>
2020-12-07 15:11:07 +0800

    代码初始化，启动应用

---
+++ readme.txt
@@ -0,0 +1,2 @@
+一、  添加application.yml 注意里面的写法，保持空格等。
+二、 filters: stat,wall,slf4j 使用slf4j,可以不用在pom中配置log4j
---
+++ src/main/java/org/felix/Easy9CopyStartUpApplication.java
@@ -0,0 +1,22 @@
+package org.felix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * 表示开启AOP代理自动配置，如果配@EnableAspectJAutoProxy表示使用cglib进行代理对象的生成；
+ * 设置@EnableAspectJAutoProxy(exposeProxy=true)表示通过aop框架暴露该代理对象
+ * 启动类
+ *
+ * @author Felix
+ */
+@SpringBootApplication
+public class Easy9CopyStartUpApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Easy9CopyStartUpApplication.class, args);
+    }
+
+}
